1d4dca9bc30358206638ed579a3c682f2b67fa46
Lordfirespeed <joe@joeclack.me.uk>
2022-10-24 18:21:34 +0100

    wait for the DOM to be ready before trying to set the active navbar element compare window location pathname, not href

--- assets/navbar.js
+++ assets/navbar.js
@@ -4,20 +4,30 @@ fetch("/assets/navbar.html")
 
 function define(html) {
     class NavBar extends HTMLElement {
-        constructor() {
-            super();
-
-            this.innerHTML = html;
-
+        on_document_loaded() {
             Array.prototype.forEach.call(
                 this.getElementsByClassName("nav-link"),
                 (nav_link_element) => {
-                    if (nav_link_element.href !== window.location.href) {return}
+                    if (nav_link_element.pathname !== window.location.pathname) {return}
+                    console.log("mug")
 
                     nav_link_element.classList.add("active");
                     nav_link_element.setAttribute("aria-current","page");
                 }
             );
+        }
+
+        constructor() {
+            super();
+
+            this.innerHTML = html;
+
+            if (document.readyState === "loading") {
+                addEventListener("DOMContentLoaded", this.on_document_loaded)
+            }
+            else {
+                this.on_document_loaded()
+            }
         }
     }
 
